4d2b82181796e92a1338f53fc1eb52669a725e81
DAN THE ANALYST <danmuendo4@gmail.com>
2026-08-24 22:17:43 +0300

    Add custom SVG favicon matching brand logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(localBusinessSchema) }}
